bab343e24597b00a7f9fcc745a6d213a3a381490
venkatesh Pittala <venkirowling@gmail.com>
2017-07-26 21:00:22 -0400

    " first commit "

---
+++ project1/src/main/java/epicsystems/sprint1/MyFirstBuild.java
@@ -0,0 +1,12 @@
+package epicsystems.sprint1;
+
+public class MyFirstBuild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My First Build is succesfull");
+
+	}
+
+}
